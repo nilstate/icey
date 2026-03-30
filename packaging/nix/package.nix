23513537e2ed9eabe9fb8a90671ec83cee44f44f
Kam <hello@sourcey.com>
2026-03-30 10:37:30 +1000

    build: vendor fetchcontent deps in nix package

--- packaging/nix/package.nix
+++ packaging/nix/package.nix
@@ -2,11 +2,8 @@
 , stdenv
 , cmake
 , pkg-config
+, fetchFromGitHub
 , openssl
-, libuv
-, llhttp
-, minizip
-, zlib
 , ffmpeg ? null
 , libdatachannel ? null
 , src
@@ -15,6 +12,28 @@
 , withWebrtc ? false
 }:
 
+let
+  vendoredLibuv = fetchFromGitHub {
+    owner = "libuv";
+    repo = "libuv";
+    rev = "v1.50.0";
+    hash = "sha256-sexWRE7j8eEMi9Pu0WukcBbtC5T+QhN0Nary4L1XRXk=";
+  };
+
+  vendoredLlhttp = fetchFromGitHub {
+    owner = "nodejs";
+    repo = "llhttp";
+    rev = "release/v9.2.1";
+    hash = "sha256-PBY4kURuUpYEtZD5rQl7Lpi1735NPdzxz5i2LKZo8j4=";
+  };
+
+  vendoredZlib = fetchFromGitHub {
+    owner = "madler";
+    repo = "zlib";
+    rev = "v1.3.1";
+    hash = "sha256-F+iIY/NgBnKrSRgvIXKBtvxNPHYr3jYZNeQ2qVIU0Fw=";
+  };
+in
 stdenv.mkDerivation {
   pname = "icey";
   inherit version src;
@@ -29,17 +48,13 @@ stdenv.mkDerivation {
   buildInputs =
     [
       openssl
-      libuv
-      llhttp
-      minizip
-      zlib
     ]
     ++ lib.optional withFfmpeg ffmpeg
     ++ lib.optional withWebrtc libdatachannel;
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=ON"
-    "-DUSE_SYSTEM_DEPS=ON"
+    "-DUSE_SYSTEM_DEPS=OFF"
     "-DBUILD_TESTS=OFF"
     "-DBUILD_SAMPLES=OFF"
     "-DBUILD_APPLICATIONS=OFF"
@@ -53,6 +68,9 @@ stdenv.mkDerivation {
     "-DWITH_LIBDATACHANNEL=${if withWebrtc then "ON" else "OFF"}"
     "-DBUILD_MODULE_webrtc=${if withWebrtc then "ON" else "OFF"}"
     "-DWITH_OPENCV=OFF"
+    "-DFETCHCONTENT_SOURCE_DIR_LIBUV=${vendoredLibuv}"
+    "-DFETCHCONTENT_SOURCE_DIR_LLHTTP=${vendoredLlhttp}"
+    "-DFETCHCONTENT_SOURCE_DIR_ZLIB=${vendoredZlib}"
   ];
 
   doCheck = false;
